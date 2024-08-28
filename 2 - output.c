95bf56b135a85a340884e7cbdecc9f44eb1d9cff
Jonas Miqueias <jhonasbola2@gmail.com>
2024-08-28 00:09:33 -0300

    Introducao ao C e Sintaxe

---
+++ 2 - output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("------------\n");
+    printf("    MENU    \n");
+    printf("------------\n\n");
+
+    /*scape sequences*/
+    printf("Ola \t ola\n");
+    printf("Mulher\\Homem\n");
+    printf("Eles o chamam de \"Jonas\".\n");
+
+    return 0;
+}
