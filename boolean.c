9851ec2949e0e2b6ad31b5d580e7f90fddb37d12
Jonas Miqueias <jhonasbola2@gmail.com>
2024-08-30 18:12:38 -0300

    Constantes, operadores, boolean

---
+++ boolean.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+    bool apto = true;
+
+    if (apto == 1){
+        printf("Verdadeiro");
+    }else{
+        printf("Falso");
+    }
+
+
+    return 0;
+}
